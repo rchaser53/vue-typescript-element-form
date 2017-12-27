32166f49097ffff7a7deafbef11592e37296ba97
rchaser53 <tayoshizawa29@gmail.com>
2017-12-27 18:02:16 +0900

    set up minimum size

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
-module.exports = require("./config/webpack.config.dev");
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules|vue\/src/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          esModule: true
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
