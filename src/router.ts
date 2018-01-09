d9ebe8ef4c39c3ff0bcdfffc389f190fc697285d
rchaser53 <tayoshizawa29@gmail.com>
2018-01-09 15:13:30 +0900

    add the sentence kinda working
    actually i cannnot recommend vee-validate
    (element also isn't  a good but better than vee-validate)

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ElementForm from './components/ElementForm.vue';
-import VeeForm from './components/VeeValidateForm.vue';
+
+const VeeForm = Vue.extend({
+	template: '<div>i cannot recommend it. use Element form</div>'
+})
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
 	mode: 'history',
 	routes: [
-		{ path: '/element', component: ElementForm }
-		// { path: '/vee', component: VeeForm }
+		{ path: '/element', component: ElementForm },
+		{ path: '/vee', component: VeeForm }
 	]
 });
